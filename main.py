7f250daaae6bff9b9a47d37ef59d9d9399d37dfb
vorlie <vorlie0961@gmail.com>
2024-08-04 19:21:56 +0200

    add missing strings

--- main.py
+++ main.py
@@ -11,36 +11,41 @@ setup_logging()
 CONFIG_PATH = "config.json"
 DEFAULT_ACCENT_COLOR = "#ff50aa"
 default_settings = {
-    "connect_to_discord_comment": {
-        "1":"Connect to Discord? If set to true, application will try to connect to discord in order to set your presence.",
-        "2":"Set it to false if you don't want to connect to Discord.",
-        "3":"The default value is 'true'."
+    "discord_comments": {
+        "1": "Connect to Discord? If set to true, application will try to connect to discord in order to set your presence.",
+        "2": "Set it to false if you don't want to connect to Discord.",
+        "3": "The default value is 'true'.",
+        "4": "Enter your Discord Client ID. You can find it in your Discord Developer Portal.",
+        "5": {
+            "1":"Enter your Discord Large Image Key. You can find it in your Discord Developer Portal.",
+            "2":"You can use links as well.",
+            "3":"The default value is 'https://i.pinimg.com/564x/d5/ed/93/d5ed93e12eab198b830bc91f1ddf2dcb.jpg'."
+        }
     },
     "connect_to_discord": True,
-        
+    "discord_client_id": "1150680286649143356",
+    "large_image_key": "https://i.pinimg.com/564x/d5/ed/93/d5ed93e12eab198b830bc91f1ddf2dcb.jpg",
+
     "root_playlist_folder_comment": {
-        "1":"Specify the directory where your playlists are saved. You can change this path if needed.", 
+        "1": "Specify the directory where your playlists are saved. You can change this path if needed.",
         "2": "The default value is 'playlists'."
     },
     "root_playlist_folder": "C:\\Users\\karol\\Music\\",
-
     "default_playlist_comment": "Enter the name of the default playlist that will be loaded initially. You can modify this name as required.",
-    "default_playlist": "default",
-
+    "default_playlist": "Nightcore",
     "colorization_color_comment": {
         "1": "Enter the hex color code of the accent color. You can modify this color as required.",
         "2": "If set to 'automatic' the application will use the system's accent color.",
         "3": "The default value is 'automatic'."
     },
     "colorization_color": "automatic",
-
     "auto_color_interval_comment": {
         "1": "Enter the interval in seconds at which the automatic color will change.",
         "2": "If you find that application is using too much resources (CPU mostly), you can increase this value. 1000 is 1 second, 10000 is 10 seconds, etc.",
         "3": "If you do increase, just so you know that it will have a bit of delay to change the color :)",
         "4": "The default value is '1000'."
     },
-    "auto_color_interval": 1000
+    "auto_color_interval": 10000
 }
 
 class AccentThread(QThread):

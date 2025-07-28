d1ac57098be70c12f3b32eda6691a2e2f338ad5b
vorlie <vorlie0961@gmail.com>
2025-07-28 19:27:30 +0200

    Bump version to 1.10.6

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ if platform.system() == "Windows":
 else:
     ICON_PATH = os.path.join(BASE_DIR, 'icon.png')
 
-__version__ = "1.10.5"
+__version__ = "1.10.6"
 
 default_settings = {
     "connect_to_discord": True,

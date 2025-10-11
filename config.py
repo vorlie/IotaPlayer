82dc3264a4346c25cf5cc39eef6de11b8bd6e886
vorlie <vorlie0961@gmail.com>
2025-10-12 01:40:41 +0200

    bump version to 1.10.13

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ if platform.system() == "Windows":
 else:
     ICON_PATH = os.path.join(BASE_DIR, 'icon.png')
 
-__version__ = "1.10.12"
+__version__ = "1.10.13"
 
 default_settings = {
     "connect_to_discord": True,
